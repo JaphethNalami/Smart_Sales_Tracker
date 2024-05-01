01c0e5d0cdd583e6c4a60e3ec97f78b43db23597
15787 <15787000000a@gmail.com>
2024-05-01 22:36:19 +0300

    Eleventh commit with few changes to the cart card layout.

--- app/src/main/java/com/example/smartsalestracker/Items_Cart.java
+++ app/src/main/java/com/example/smartsalestracker/Items_Cart.java
@@ -6,7 +6,11 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -26,7 +30,13 @@ public class Items_Cart extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_items_cart);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         back = findViewById(R.id.back_button);
         clear_cart = findViewById(R.id.clear_cart);
