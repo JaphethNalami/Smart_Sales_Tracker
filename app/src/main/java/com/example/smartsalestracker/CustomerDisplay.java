2c57bd9b5f7e4467377ed1876da4808bac5792c6
Wagatwe Maina <mjosphine52@gmail.com>
2024-07-23 12:23:43 +0300

    customer display

--- app/src/main/java/com/example/smartsalestracker/CustomerDisplay.java
+++ app/src/main/java/com/example/smartsalestracker/CustomerDisplay.java
@@ -36,38 +36,39 @@ public class CustomerDisplay extends AppCompatActivity {
     FloatingActionButton fab;
     ImageButton back;
 
-    private FirebaseFirestore db ;
+    private FirebaseFirestore db;
     private FirebaseUser user;
     private String userId;
     private FirebaseAuth mAuth;
     Dialog dialog;
-    ArrayList<Customer>customerArrayList;
+    ArrayList<Customer> customerArrayList;
     CustomerDetailsAdapter customerAdapter;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_customer_display);
+
+        // Set window insets to avoid system bars overlay
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        //initialize the views
+        // Initialize the views
         recyclerView = findViewById(R.id.recyclerView);
         fab = findViewById(R.id.refresh);
         back = findViewById(R.id.back);
 
-        //initialize the firebase
+        // Initialize Firebase instances
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
         userId = Objects.requireNonNull(user).getUid();
 
-        //dialog
+        // Initialize and show a dialog to indicate data retrieval is in progress
         dialog = new MaterialAlertDialogBuilder(this)
                 .setView(new ProgressBar(this))
                 .setTitle("Fetching Data...")
@@ -75,30 +76,32 @@ public class CustomerDisplay extends AppCompatActivity {
                 .create();
         dialog.show();
 
+        // Set a click listener for the FloatingActionButton to refresh data
         fab.setOnClickListener(v -> {
             customerArrayList.clear();
             getCustomerDetails();
         });
 
+        // Set a click listener for the back button to finish the activity
         back.setOnClickListener(v -> {
             finish();
         });
 
+        // Setup the RecyclerView with a linear layout manager and adapter
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // Initialize the ArrayList and adapter
         customerArrayList = new ArrayList<>();
         customerAdapter = new CustomerDetailsAdapter(CustomerDisplay.this, customerArrayList);
         recyclerView.setAdapter(customerAdapter);
 
-        //call method to get customer details from the database
+        // Call method to get customer details from the database
         getCustomerDetails();
-
     }
 
     private void getCustomerDetails() {
-
-        // Show a dialog indicating data retrieval is in progress
+        // Show the dialog indicating data retrieval is in progress
         dialog.show();
 
         db.collection(userId).document("Shop").collection("Customers_Details").addSnapshotListener((new EventListener<QuerySnapshot>() {
@@ -115,7 +118,9 @@ public class CustomerDisplay extends AppCompatActivity {
                 customerArrayList.clear(); // Clear existing data
 
                 assert value != null;
+                // Add new data from the snapshot to the list
                 customerArrayList.addAll(value.toObjects(Customer.class));
+                // Sort the list based on customer period
                 customerArrayList.sort((o1, o2) -> o2.getCustomerPeriod().compareTo(o1.getCustomerPeriod()));
                 // Notify the adapter that the data set has changed
                 customerAdapter.notifyDataSetChanged();
@@ -124,9 +129,8 @@ public class CustomerDisplay extends AppCompatActivity {
                     dialog.dismiss();
                 }
 
-                // You may perform other operations here after data retrieval
+                // Additional operations after data retrieval can be performed here
             }
         }));
-
     }
 }
